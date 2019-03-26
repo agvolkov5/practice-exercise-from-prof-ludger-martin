f56ce1eb10860c1ce053e8ee778dad7ead99bfe3
Alexander Volkov <agvolkov4@yandex.ru>
2019-03-26 20:52:34 +0300

    column drop; grid setted

--- gps.js
+++ gps.js
@@ -1,11 +1,14 @@
 const $blue = document.getElementById('blue');
-function successCallback(position) {
+
+successCallback = (position) => {
     $blue.innerText = 'Lat: ' + position.coords.latitude +
                         '\nLog: ' + position.coords.longitude +
                         '\nH: ' + position.coords.altitude;
-}
-function errorCallback(error) {
+};
+
+errorCallback = (error) => {
     console.log(error);
     $blue.innerText = 'Ehh.. You don\'t allow permission for GPS :(';
-}
+};
+
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
